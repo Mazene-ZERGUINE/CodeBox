ff354b7cb552a632473e59a8e54c4d7cf0623d00
Mazene <mmazenezerguine@gmail.com>
2025-09-01 15:31:50 +0200

    Adding postman collection

--- app/api/serializers/code_with_files_task.py
+++ app/api/serializers/code_with_files_task.py
@@ -6,8 +6,13 @@ LANG_CHOICES = ("python", "javascript", "c", "php")
 class CodeWithFilesTaskSerializer(serializers.Serializer):
     programming_language = serializers.ChoiceField(choices=LANG_CHOICES)
     source_code = serializers.CharField()
-    input_files = serializers.ListField(child=serializers.CharField(), allow_null=True)
+    input_files = serializers.ListField(
+        child=serializers.CharField(),
+        allow_null=True,
+        allow_empty=True,
+        required=False
+    )
 
     files = serializers.ListField(
-        child=serializers.FileField(), allow_empty=True
+        child=serializers.FileField(), allow_empty=True, allow_null=True, required=False
     )
